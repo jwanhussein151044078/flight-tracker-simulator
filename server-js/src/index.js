32fc7d17bec72ee3ebcce1e7aad312520fb26921
jwan hussein <jwan.hussein.1993@gmail.com>
2024-03-08 18:39:33 +0300

    Update index.js

--- server-js/src/index.js
+++ server-js/src/index.js
@@ -9,16 +9,13 @@ const { Server } = require('socket.io');
 const models = require('./models/index.js');
 require('dotenv').config();
 
-
-
 const app = express();
-const port = process.env.PORT ;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
 });
-
 
 io.on('connection', (socket) => {
     console.log('a user connected');
@@ -26,7 +23,6 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 });
-
 
 app.use(credentials);
 app.use(cors(corsOptions));
